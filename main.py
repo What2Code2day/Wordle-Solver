4af8795ae3d7705731cf85dce3dfe090d51f3c34
What2Code2day <95487916+What2Code2day@users.noreply.github.com>
2022-02-26 18:21:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# grabs the words from the word list
+def load_dict(file_name):
+    file = open(file_name)
+    words = file.readlines()
+    file.close()
+    return [word[:5] for word in words]
+
+
+words = load_dict("words.txt")
+final_word = False
+while final_word == False:
+    # asks the user for there guess
+    guess_letter = input('Input your letter: ')
+    if guess_letter == "done":
+        exit()
+    guess_color = input('What is the color(Y,G,B): ')
+    # filters for grey letters
+    if guess_color == "b" or guess_color == 'B':
+        words = list(filter(lambda s: guess_letter not in s, words))
+        print(words)
+        continue
+    letter_pos = int(input('what pos: '))
+    # filters for green letters
+    if guess_color == "G" or guess_color == "g":
+        words = list(
+            filter(lambda s: s[letter_pos-1] == guess_letter in s, words))
+        print(words)
+    # filters for yellow letters
+    elif guess_color == "y" or guess_color == "Y":
+        words = list(filter(lambda s: guess_letter in s, words))
+        words = [
+            word
+            for word in words
+            if word[letter_pos-1] != guess_letter
+        ]
+        print(words)
+    print('-------------------------------------------------------------------------')
