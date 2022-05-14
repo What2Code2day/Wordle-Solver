6c1427299c8bd80bdecaa4a8a3dc53015145aa00
Jake <95487916+What2Code2day@users.noreply.github.com>
2022-05-14 15:02:01 -0400

    Made it so it prints a str not a list
    Also added a line underneath and added a split() method to the "input a letter"

--- main.py
+++ main.py
@@ -16,22 +16,26 @@ while final_word == False:
     guess_color = input('What is the color(Y,G,B): ')
     # filters for grey letters
     if guess_color == "b" or guess_color == 'B':
-        words = list(filter(lambda s: guess_letter not in s, words))
-        print(words)
+        words = list(filter(lambda s: guess_letter.strip() not in s, words))
+        print(', '.join(words))
+        print('-------------------------------------------------------------------------')
         continue
     letter_pos = int(input('what pos: '))
     # filters for green letters
     if guess_color == "G" or guess_color == "g":
         words = list(
-            filter(lambda s: s[letter_pos-1] == guess_letter in s, words))
-        print(words)
+            filter(lambda s: s[letter_pos-1] == guess_letter.strip() in s, words))
+        print(", ".join(words))
+        print('-------------------------------------------------------------------------')
+        continue
     # filters for yellow letters
     elif guess_color == "y" or guess_color == "Y":
-        words = list(filter(lambda s: guess_letter in s, words))
+        words = list(filter(lambda s: guess_letter.strip() in s, words))
         words = [
             word
             for word in words
             if word[letter_pos-1] != guess_letter
         ]
-        print(words)
+        print(', '.join(words))
+        print('-------------------------------------------------------------------------')
     print('-------------------------------------------------------------------------')
