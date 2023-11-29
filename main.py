3679f30d381bd4bb14ae54d66df8c1817b98298c
jakeharrison <heyJakeHarrison@gmail.com>
2023-11-29 14:45:20 -0500

    Optimized the code and made it more readable

--- main.py
+++ main.py
@@ -1,41 +1,64 @@
 # grabs the words from the word list
 def load_dict(file_name):
-    file = open(file_name)
-    words = file.readlines()
-    file.close()
-    return [word[:5] for word in words]
+    with open(file_name) as f:
+        words = f.readlines()
+    return [word.strip() for word in words]
 
 
-words = load_dict("words.txt")
-final_word = False
-while final_word == False:
+# filters for grey letters
+def gray_filter(guess_letter):
+    words = [word for word in words if guess_letter.strip().lower() not in word]
+    print(', '.join(words))
+    print('-------------------------------------------------------------------------')
+    return words
+
+
+# filters for green letters
+def green_filter(guess_letter):
+    letter_pos = int(input('what position: '))
+    words = [word for word in words if word[letter_pos-1] == guess_letter.strip().lower()]
+    print(', '.join(words))
+    print('-------------------------------------------------------------------------')
+    return words
+
+
+# filters for yellow letters
+def yellow_filter(guess_letter):
+    letter_pos = int(input('what position: '))
+    words = [word for word in words if word[letter_pos-1] != guess_letter.strip().lower()]
+    print(', '.join(words))
+    print('-------------------------------------------------------------------------')
+    return words
+
+def check_done():
+    if len(words) <= 1:
+        exit()
+    
+
+words = load_dict("Wordle-Solver/words.txt")
+while True:
+
+    # checks if the user is done
+    check_done()
+
     # asks the user for there guess
     guess_letter = input('Input your letter: ')
-    if guess_letter == "done":
-        exit()
-    guess_color = input('What is the color(Y,G,B): ')
+
+    # asks the user for the color of the letter
+    guess_color = input('What is the color (Y,G,B): ')
+
     # filters for grey letters
-    if guess_color == "b" or guess_color == 'B':
-        words = list(filter(lambda s: guess_letter.strip() not in s, words))
-        print(', '.join(words))
-        print('-------------------------------------------------------------------------')
-        continue
-    letter_pos = int(input('what pos: '))
+    if guess_color.lower() == "b":
+        gray_filter(guess_letter)
+    
     # filters for green letters
-    if guess_color == "G" or guess_color == "g":
-        words = list(
-            filter(lambda s: s[letter_pos-1] == guess_letter.strip() in s, words))
-        print(", ".join(words))
-        print('-------------------------------------------------------------------------')
-        continue
+    if guess_color.lower()=="g":
+        words = green_filter(guess_letter)
+
     # filters for yellow letters
-    elif guess_color == "y" or guess_color == "Y":
-        words = list(filter(lambda s: guess_letter.strip() in s, words))
-        words = [
-            word
-            for word in words
-            if word[letter_pos-1] != guess_letter
-        ]
-        print(', '.join(words))
-        print('-------------------------------------------------------------------------')
+    if guess_color.lower()=="y":
+        words = yellow_filter(guess_letter)
+    
+    # Separates the while loops
     print('-------------------------------------------------------------------------')
+
