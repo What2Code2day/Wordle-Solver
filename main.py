921419fbcfcfa97651046d69d273d7048b52221c
jakeharrison <heyJakeHarrison@gmail.com>
2023-11-30 02:37:01 -0500

    Fixed Bug

--- main.py
+++ main.py
@@ -4,38 +4,37 @@ def load_dict(file_name):
         words = f.readlines()
     return [word.strip() for word in words]
 
+global words
+words = load_dict("Wordle-Solver/words.txt")
 
 # filters for grey letters
 def gray_filter(guess_letter):
+    global words
     words = [word for word in words if guess_letter.strip().lower() not in word]
     print(', '.join(words))
     print('-------------------------------------------------------------------------')
-    return words
-
 
 # filters for green letters
 def green_filter(guess_letter):
+    global words
     letter_pos = int(input('what position: '))
     words = [word for word in words if word[letter_pos-1] == guess_letter.strip().lower()]
     print(', '.join(words))
     print('-------------------------------------------------------------------------')
-    return words
-
 
 # filters for yellow letters
 def yellow_filter(guess_letter):
+    global words
     letter_pos = int(input('what position: '))
     words = [word for word in words if word[letter_pos-1] != guess_letter.strip().lower()]
     print(', '.join(words))
     print('-------------------------------------------------------------------------')
-    return words
 
 def check_done():
+    global words
     if len(words) <= 1:
         exit()
-    
 
-words = load_dict("Wordle-Solver/words.txt")
 while True:
 
     # checks if the user is done
@@ -53,12 +52,11 @@ while True:
     
     # filters for green letters
     if guess_color.lower()=="g":
-        words = green_filter(guess_letter)
+        green_filter(guess_letter)
 
     # filters for yellow letters
     if guess_color.lower()=="y":
-        words = yellow_filter(guess_letter)
+        yellow_filter(guess_letter)
     
     # Separates the while loops
     print('-------------------------------------------------------------------------')
-
